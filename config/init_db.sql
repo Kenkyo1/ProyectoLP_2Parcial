e8cd8a6ccbd3f2528d92c8d1ca245095034d9dfe
Angel Ariel Gómez Tumbaco <116834047+Kenkyo1@users.noreply.github.com>
2025-09-03 09:39:04 -0500

    Update init_db.sql

--- config/init_db.sql
+++ config/init_db.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS usuarios (
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    is_admin TINYINT(1) NOT NULL DEFAULT 0,
+    rol VARCHAR(50) NOT NULL DEFAULT 'usuario'
 );
 
 CREATE TABLE IF NOT EXISTS denuncias (
@@ -18,4 +20,8 @@ CREATE TABLE IF NOT EXISTS denuncias (
     imagen VARCHAR(255),
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+    estado VARCHAR(20) NOT NULL DEFAULT 'Pendiente'
 );
+
+INSERT INTO usuarios (nombre, email, password, fecha_registro, is_admin, rol)
+VALUES ('Administrador Principal', 'admin@ecoalert.com', '$2y$10$ZBKkP3dSIQFFtQAk1f9F6eX25rtM7zyzRiLBPAHzPv7nb668fU2My', NOW(), 1, 'administrador');
